7faa9a7df6ba8729bef40836cd49224654991306
baleta <mateusbalabenute@yahoo.com.au>
2022-06-28 00:45:04 -0300

    (fix) args

--- macwizard.py
+++ macwizard.py
@@ -5,10 +5,10 @@ import subprocess
 import os
 
 dona = """
-===================== Made by BraiNiac ==================
-===================== Buy me a beer :) ==================
-==== BTC: bc1q8z64uky7jgwdsygc7fwq97d4u8yfr8hj57s200 ====
-=========================================================
+ ===================== Made by BraiNiac ==================
+ ===================== Buy me a beer :) ==================
+ ======== BTC: 1Lw3QxNkoBoebZdSYHran251FzUrkewHgN ========
+ =========================================================
 """
 f = Figlet(font='banner3-D')
 
@@ -17,21 +17,46 @@ print(colored(f.renderText('Wizard'), 'green'))
 
 print(dona)
 
-parser = ArgumentParser(description='macchanger [Option] ', usage='python3 macchanger --help',
-                        epilog='[ Set Manually ] python3 macwizard.py -i [interface] -m [YY:YY:YY:YY:YY:YY]')
+parser = ArgumentParser(description='macwizard [Option] ',
+                        usage='python macwizard --help',
+                        epilog='[ Set Manually ] python3 macwizard.py \
+                            -i [interface] -m [YY:YY:YY:YY:YY:YY]')
 
-rparser = parser.add_argument_group('Required Arguments:')
+parser.add_argument_group('Required Arguments:')
 
-rparser.add_argument('-i', '--interface', dest='iface', metavar='',
-                     type=str, help='iface you want to change the MAC')
-rparser.add_argument('-m', '--mac', dest='newmac', metavar='',
-                     type=str, help='MAC address to change MANUALLY')
-rparser.add_argument(
-    '-s', '--show', help='Show available ifaces and exit script', action='store_true')
 parser.add_argument(
-    '-R', '--Random', help='Generate a random MAC', action='store_true')
+    '-i', '--interface',
+    dest='iface',
+    metavar='',
+    type=str,
+    help='iface you want to change the MAC'
+)
+
 parser.add_argument(
-    '-r', '--reset', help="Reset to Original MAC", action="store_true")
+    '-m', '--mac',
+    dest='newmac',
+    metavar='',
+    type=str,
+    help='MAC address to change MANUALLY'
+)
+
+parser.add_argument(
+    '-s', '--show',
+    help='Show available ifaces and exit script',
+    action='store_true'
+)
+
+parser.add_argument(
+    '-R', '--Random',
+    help='Generate a random MAC',
+    action='store_true'
+)
+
+parser.add_argument(
+    '-r', '--reset',
+    help="Reset to Original MAC",
+    action="store_true"
+)
 
 args = parser.parse_args()
 
@@ -59,7 +84,3 @@ def changemac():
         subprocess.call(['sudo', 'rm', '-rf', '.test.txt'])
     else:
         subprocess.call(['python3', 'macwizard.py', '-h'])
-
-
-if __name__ == '__main__':
-    changemac()
